57a53019d809ba551a52c48f49f6b400ba28a871
lazyneko <547592574@qq.com>
2020-07-12 23:58:02 +0800

    添加 request 目录，添加 plugin 描述

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,12 @@
+/**
+ * 扩展 vue 功能时建议在对应文件添加扩展声明
+ * declare module "vue/types/vue" {
+ *  interface Vue {
+ *    [extension: string]: Function
+ *  }
+ * }
+ */
+
 import { VueConstructor, PluginObject } from "vue";
 
 function loadPlugins(Vue: VueConstructor) {
